dcc229485d722aee363aa99517183cc5f1b2cb85
Joel Martelleur <joel.martelleur@gmail.com>
2024-05-28 19:38:45 +0200

    Implementing person class with validation

---
+++ Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿using Task3;
+
+try
+{
+    var p1 = new Person("Jon", "Mayer") { Age = 1 };
+    Console.WriteLine($"""
+        Person: {p1.FName} {p1.LName} age {p1.Age}
+    """);
+}
+catch (ArgumentException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
